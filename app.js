4dfd743b1813ca3a93267907f36a1b5c64b406b4
Bruce Zhu <brucezzhu@gmail.com>
2022-04-09 10:44:46 +1000

    change routes name

--- app.js
+++ app.js
@@ -24,14 +24,14 @@ app.get('/', (req, res) => {
 })
 
 // aboutUs page
-app.get('/aboutUs', (req, res) => {
+app.get('/about-us', (req, res) => {
     res.render('about/aboutUs', {
         style: 'about.css'
     });
 })
 
 // aboutDiabetes page
-app.get('/aboutDiabetes', (req, res) => {
+app.get('/about-diabetes', (req, res) => {
     res.render('about/aboutDiabetes', {
         style: 'about.css'
     });
