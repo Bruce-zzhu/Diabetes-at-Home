aa521a38494cdf04b53d77d3637a4a3275dda24f
Vivien Guo <guov@student.unimelb.edu.au>
2022-05-07 02:10:01 +1000

    nav switch display script

--- public/scripts/view-patient.js
+++ public/scripts/view-patient.js
@@ -1,5 +1,25 @@
 function viewPatient(id) {
+    var reqCon = document.getElementById("require-container");
+    var msgCon = document.getElementById("message-container");
+    var ntsCon = document.getElementById("notes-container");
 
+    switch (id) {
+        case "require":
+            reqCon.style.display = "block";
+            msgCon.style.display = "none";
+            ntsCon.style.display = "none";
+            break;
+        case "message":
+            reqCon.style.display = "none";
+            msgCon.style.display = "block";
+            ntsCon.style.display = "none";
+            break;
+        case "notes":
+            reqCon.style.display = "none";
+            msgCon.style.display = "none";
+            ntsCon.style.display = "block";
+            break;
+    }
 }
 
 module.exports = {
