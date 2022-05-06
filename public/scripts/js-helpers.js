8c70bc3175683aec9f4c20f47f6385b42ead4a8f
Vivien Guo <guov@student.unimelb.edu.au>
2022-05-07 01:39:45 +1000

    corrected viewPatient table behaviour & style

--- public/scripts/js-helpers.js
+++ public/scripts/js-helpers.js
@@ -24,10 +24,29 @@ function getDateInfo(dateString) {
     return [day, month, year]
 }
 
+// display/hide a data table cell's comment/value
+function tableToggleComment(cell) {
+    var comment = cell.querySelector(".tb-comm"); 
+    var value = cell.querySelector(".tb-value");
+    if (comment.style.display != "block") {
+        comment.style.display = "block";
+        value.style.display = "none";
+        cell.style.background = "white";
+        cell.style.borderLeft = "1px solid var(--brown-color)";
+        cell.style.borderRight = "1px solid var(--brown-color)";
+    } else {
+        comment.style.display = "none";
+        value.style.display = "block";
+        cell.style.background = "var(--tertiary-color)";
+        cell.style.borderLeft = "0";
+        cell.style.borderRight = "0";
+    }
+}
 
 module.exports = {
     logOut,
     consolelogs,
     isSameDay,
-    getDateInfo
+    getDateInfo,
+    tableToggleComment
 };
