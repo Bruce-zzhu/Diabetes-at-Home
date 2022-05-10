0cf70d970df46d174ebf81d0f120413fdd284c05
Vivien Guo <guov@student.unimelb.edu.au>
2022-05-11 01:40:10 +1000

    logic for adjusting hist-table

--- public/scripts/hist-table.js
+++ public/scripts/hist-table.js
@@ -19,9 +19,69 @@ function stackToggleComment(stack) {
 }
 
 function adjust_histTable(req_count) {
+    // Adjust stacks height
     var stacks = document.getElementsByClassName("stack");
     console.log(stacks);
     for (var i=0; i<stacks.length; i++) {
         stacks[i].style.height = "calc(" + 100/req_count + "% + 0.25px)";
+    }
+    
+    // Adjust hist-table date box
+    var dateArea = document.getElementsByClassName("date-area");
+    var dayText = document.getElementsByClassName("date-day");
+    var todayDateBox = document.getElementById("hist-today").querySelector(".date-area");
+    var avgDateBox = document.getElementById("hist-7days").querySelector(".date-area");
+    switch(req_count) {
+        case 4:
+            for (i=0; i<dateArea.length; i++) {
+                dateArea[i].style.minHeight = "200px";
+            }
+            break;
+        case 3:
+            for (i=0; i<dateArea.length; i++) {
+                dateArea[i].style.minHeight = "150px";
+            }
+            for (i=0; i<dayText.length; i++) {
+                dayText[i].style.fontSize = "1.5em";
+            }
+            break;
+        case 2:
+            for (i=0; i<dateArea.length; i++) {
+                dateArea[i].style.minHeight = "100px";
+            }
+            for (i=0; i<dayText.length; i++) {
+                dayText[i].style.fontSize = "1.25em";
+            }
+
+            avgDateBox.querySelector(".date-from .date-day").innerHTML += 
+                " ~ " + avgDateBox.querySelector(".date-to .date-day").innerHTML;
+            avgDateBox.querySelector(".date-to").remove();
+            break;
+        case 1:
+            for (i=0; i<dateArea.length; i++) {
+                dateArea[i].style.minHeight = "50px";
+            }
+            for (i=0; i<dayText.length; i++) {
+                dayText[i].style.fontSize = "1em";
+            }
+            var monthText = document.getElementsByClassName("date-month");
+            for (i=0; i<monthText.length; i++) {
+                monthText[i].style.fontSize = "1em";
+            }
+
+            todayDateBox.querySelector(".date-month").innerHTML +=
+            "/ " + todayDateBox.querySelector(".date-year").innerHTML;
+            todayDateBox.querySelector(".date-year").remove()
+
+            avgDateBox.querySelector(".date-month").innerHTML +=
+            "/ " + avgDateBox.querySelector(".date-year").innerHTML;
+            avgDateBox.querySelector(".date-year").remove()
+
+            avgDateBox.querySelector(".date-from .date-day").innerHTML += 
+            " ~ " + avgDateBox.querySelector(".date-to .date-day").innerHTML;
+            avgDateBox.querySelector(".date-to").remove();
+            break;
+        default:
+
     }
 }
