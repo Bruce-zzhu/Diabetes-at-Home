2902c22cab2e0c6580cf4ed324ad2eff9fa46e3d
Vivien Guo <guov@student.unimelb.edu.au>
2022-05-11 13:43:13 +1000

    updated adjust histTable logic

--- public/scripts/p-dashboard.js
+++ public/scripts/p-dashboard.js
@@ -1,13 +1,8 @@
-// console.log(required);
-
-// hide non-required stats
+var tb = document.getElementById("full-history");
 for (var stat in required) {
-    // console.log(stat);
-    // console.log(required[stat]);
-
     // rm item if the property is not required
     if (required[stat] === false) {
-        var stat_objs = document.getElementsByClassName(stat);
+        var stat_objs = tb.getElementsByClassName(stat);
         for (var i=0; i<stat_objs.length; i++) {
             stat_objs[i].style.display = "none";
         }
@@ -16,4 +11,18 @@ for (var stat in required) {
 
 var req_count = Object.values(required).reduce((req_count, stat) => (req_count += stat));
 
-adjust_histTable(req_count);
+switch (window.innerWidth <= 375) {
+    
+    case true:
+        mobile_adjust_histTable(req_count);
+
+        break;
+
+    case false:
+        tab_desk_adjust_histTable(req_count);
+        break;
+
+    default:
+        // pass
+}
+
