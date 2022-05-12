a6902054d8caf12a99b3f326c7a98a79be936718
Vivien Guo <guov@student.unimelb.edu.au>
2022-05-12 21:37:41 +1000

    dark theme draft 1

--- public/scripts/js-helpers.js
+++ public/scripts/js-helpers.js
@@ -57,8 +57,8 @@ function setTheme(themeName) {
         case "dark":
             rootStyle.setProperty("--bg-color", "var(--brown-color)");
             rootStyle.setProperty("--border-color", "var(--offWhite-color)");
-            rootStyle.setProperty("--text-color", "white");
-            rootStyle.setProperty("--primary-color", "black");
+            rootStyle.setProperty("--text-color", "var(--offWhite-color)");
+            rootStyle.setProperty("--primary-color", "var(--offBlack-color");
             rootStyle.setProperty("--secondary-color", "var(--brown-color)");
             rootStyle.setProperty("--tertiary-color", "var(--lightGrey-color)");
             break;
