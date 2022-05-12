d7ac166028e231e3c2b608999e28e5dcf83f0754
Vivien Guo <guov@student.unimelb.edu.au>
2022-05-12 23:48:19 +1000

    more theme changes

--- public/scripts/js-helpers.js
+++ public/scripts/js-helpers.js
@@ -37,7 +37,7 @@ function tableToggleComment(cell) {
     } else {
         comment.style.display = "none";
         value.style.display = "block";
-        cell.style.background = "var(--tertiary-color)";
+        cell.style.background = "var(--secondary-color)";
         cell.style.borderLeft = "0";
         cell.style.borderRight = "0";
     }
@@ -51,9 +51,10 @@ function setTheme(themeName) {
             rootStyle.setProperty("--border-color", "var(--grey-color)");
             rootStyle.setProperty("--text-color", "black");
             rootStyle.setProperty("--alt-text-color", "white");
+            rootStyle.setProperty("--button-color", "var(--lightBlue-color)");
             rootStyle.setProperty("--primary-color", "var(--blue-color)");
-            rootStyle.setProperty("--secondary-color", "var(--lightBlue-color)");
-            rootStyle.setProperty("--tertiary-color", "var(--offGrey-color)");
+            rootStyle.setProperty("--secondary-color", "var(--offGrey-color)");
+            rootStyle.setProperty("--tertiary-color", "white");
             var logos = document.getElementsByClassName("logo");
             for (var i=0; i<logos.length; i++) {
                 logos[i].src = "/images/logo-white.svg"
@@ -64,9 +65,10 @@ function setTheme(themeName) {
             rootStyle.setProperty("--border-color", "var(--offWhite-color)");
             rootStyle.setProperty("--text-color", "white");
             rootStyle.setProperty("--alt-text-color", "white");
+            rootStyle.setProperty("--button-color", "var(--grey-color)");
             rootStyle.setProperty("--primary-color", "var(--offBlack-color");
-            rootStyle.setProperty("--secondary-color", "var(--grey-color)");
-            rootStyle.setProperty("--tertiary-color", "var(--lightGrey-color)");
+            rootStyle.setProperty("--secondary-color", "var(--lightGrey-color)");
+            rootStyle.setProperty("--tertiary-color", "var(--grey-color)");
             var logos = document.getElementsByClassName("logo");
             for (var i=0; i<logos.length; i++) {
                 logos[i].src = "/images/logo-white.svg"
@@ -77,9 +79,10 @@ function setTheme(themeName) {
             rootStyle.setProperty("--border-color", "var(--grey-color)");
             rootStyle.setProperty("--text-color", "black");
             rootStyle.setProperty("--alt-text-color", "black");
+            rootStyle.setProperty("--button-color", "white");
             rootStyle.setProperty("--primary-color", "white");
-            rootStyle.setProperty("--secondary-color", "white");
-            rootStyle.setProperty("--tertiary-color", "var(--offWhite-color)");
+            rootStyle.setProperty("--secondary-color", "var(--offWhite-color)");
+            rootStyle.setProperty("--tertiary-color", "white");
             var logos = document.getElementsByClassName("logo");
             for (var i=0; i<logos.length; i++) {
                 logos[i].src = "/images/logo-black.svg"
