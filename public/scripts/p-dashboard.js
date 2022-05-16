ca80de06f1a117ed11c1bb6295a3ed962f33e14e
Vivien Guo <guov@student.unimelb.edu.au>
2022-05-16 23:10:27 +1000

    responsive size change + minor tweaks

--- public/scripts/p-dashboard.js
+++ public/scripts/p-dashboard.js
@@ -1,6 +1,6 @@
 var req_count = Object.values(required).reduce((req_count, stat) => (req_count += stat));
 
-switch (window.innerWidth <= 375) {
+switch (window.innerWidth <= 380) {
     
     case true:
         mobile_adjust_histTable(req_count);
