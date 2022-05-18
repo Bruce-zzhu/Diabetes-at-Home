f7114efc6cfa147ad9c97cafd8863ae1d8054f96
Vivien Guo <guov@student.unimelb.edu.au>
2022-05-18 14:40:08 +1000

    minor styling

--- public/scripts/set-password.js
+++ public/scripts/set-password.js
@@ -1,21 +1,29 @@
 function emailWithValue(){
+    var subBtn = document.getElementById("email-submit");
     if (document.getElementById("email").value != ""){
-        document.getElementById("email-submit").style.backgroundColor = "var(--green-color)"
-        document.getElementById("email-submit").style.cursor = "pointer"
+        subBtn.style.backgroundColor = "var(--green-color)"
+        subBtn.style.color = "white"
+        subBtn.style.cursor = "pointer"
     } else{
-        document.getElementById("email-submit").style.backgroundColor = "var(--border-color)"
-        document.getElementById("email-submit").style.cursor = "default"
+        subBtn.style.backgroundColor = "var(--border-color)"
+        subBtn.style.color = "var(--altText-color)"
+        subBtn.style.cursor = "default"
     }
 }
 
 function passwordWithValue(){
     var pw1 = document.getElementById("password").value;
     var pw2 = document.getElementById("second-password").value;
+    var subBtn = document.getElementById("reset-submit");
     if ((pw1 != "") && (pw2 != "") && (pw1 === pw2)) {
-        document.getElementById("reset-submit").style.backgroundColor = "var(--green-color)"
-        document.getElementById("reset-submit").style.cursor = "pointer"
+        subBtn.style.backgroundColor = "var(--green-color)"
+        subBtn.style.color = "white"
+        subBtn.style.cursor = "pointer"
+        subBtn.disabled = false;
     } else {
-        document.getElementById("reset-submit").style.backgroundColor = "var(--border-color)"
-        document.getElementById("reset-submit").style.cursor = "default"
+        subBtn.style.backgroundColor = "var(--border-color)"
+        subBtn.style.color = "var(--altText-color)"
+        subBtn.style.cursor = "default"
+        subBtn.disabled = true;
     }
 }
