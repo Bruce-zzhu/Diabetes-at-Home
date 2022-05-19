14f9a31e2d31c92b7a9fbc0e0d802f3c4d7d54d8
Bruce Zhu <brucezzhu@gmail.com>
2022-05-19 19:20:00 +1000

    change some styles

--- app.js
+++ app.js
@@ -117,7 +117,6 @@ app.all('*', (req, res, next) => {
 
 // Error handler
 app.use((err, req, res, next) => {
-    
     const { statusCode = 500 } = err;
     if(!err.message) err.message = "Oh No, Something Went Wrong!";
     res.status(statusCode).render('error', { err });
