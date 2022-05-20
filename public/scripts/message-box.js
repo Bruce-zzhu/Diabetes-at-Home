ca318c69ac11647235bf7ed79edf0a9dc31cfeb4
Vivien Guo <guov@student.unimelb.edu.au>
2022-05-20 12:20:47 +1000

    fixed new message notif display bug

--- public/scripts/message-box.js
+++
@@ -1,6 +0,0 @@
-// displays unread notif if any unread messages present
-var unread = document.getElementsByClassName("unread");
-if (unread.length === 0) {
-    var notif = document.getElementById("unread-notif");
-    if (notif) {notif.remove();}
-}
